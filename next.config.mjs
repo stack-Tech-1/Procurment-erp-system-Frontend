1f667d7ee16e8eb99a9a9032b98fdd59649131d3
stack-Tech-1 <stacktechnologies0@gmail.com>
2025-10-25 02:32:10 +0100

    build change 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // ✅ new way to enable static export
     images: {
-      unoptimized: true, // ✅ needed for static export
+      unoptimized: true,
     },
   };
   
   export default nextConfig;
+  
   
